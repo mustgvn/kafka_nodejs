69f1ff97ba1a1b980acdab10c033f961f3facfbf
Mustafa Güven <mustafaguven_97@hotmail.com>
2022-01-09 17:20:06 +0300

    mustgvn fix

--- kafka-cassandra/consumer.js
+++ kafka-cassandra/consumer.js
@@ -42,7 +42,7 @@ async function createConsumer() {
                 console.log(
                     `Gelen Mesaj ${result.message.value}, Par => ${result.partition}, Count : ${count}`
                 );
-        
+
                 getCustomers();
 
             }
@@ -53,28 +53,34 @@ async function createConsumer() {
     }
 
 }
-function getCustomers(){
+function getCustomers() {
     try {
-        const query = 'INSERT INTO example_keyspace.metrics(id, assetid, cpuload, cpuusage, diskusage, memusage) VALUES ('+create_UUID()+', '+getrandom()+','+getrandom()+','+getrandom()+','+getrandom()+','+getrandom()+');';
-        client.execute(query, null, function (err, result) {
-            console.log('User with email %s', result);
-        });
-    
-    
+
+
+
+        const query = 'INSERT INTO example_keyspace.metrics(id, assetid, cpuload, cpuusage, diskusage, memusage) VALUES (?,?,?,?,?,?);';
+        const params = [create_UUID(), getrandom(), getrandom(), getrandom(), getrandom(), getrandom()];
+
+
+        //const query = 'INSERT INTO example_keyspace.metrics(id, assetid, cpuload, cpuusage, diskusage, memusage) VALUES (' + create_UUID() + ', ' + getrandom() + ',' + getrandom() + ',' + getrandom() + ',' + getrandom() + ',' + getrandom() + ');';
+        client.execute(query, params, { prepare: true })
+            .then(result => console.log('Row insert on the cluster',result));
+
+
     } catch (error) {
         console.log("Bir hata Oluştu -->", error)
     }
 }
 
-function create_UUID(){
+function create_UUID() {
     var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (dt + Math.random()*16)%16 | 0;
-        dt = Math.floor(dt/16);
-        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
     return uuid;
 }
-function getrandom(){
-   return Math.floor(Math.random() * 100);
+function getrandom() {
+    return Math.floor(Math.random() * 100);
 }
